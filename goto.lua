da3d2168889744cf2326476ad3ed30bb83649ad9
Aidan Murray <aidanm0121@gmail.com>
2020-11-29 01:08:40 -0500

    coor thing work, turtle go zooooom

--- goto.lua
+++ goto.lua
@@ -1,23 +1,63 @@
--- I stole this code but it dont work
-function axis()
-    x1 , y1, z1 = gps.locate( 2, false )
+function fowardNum(blocks)
+  for i=1, blocks do
     while turtle.detect() do
       turtle.dig()
-        end
-        turtle.forward()
-    x2 , y2 , z3=gps.locate( 2, false )
-    if x2-x1 == 1 and y2-y1 ==0 then
-      dir="X"
-    elseif x2-x1 == -1 and y2-y1 ==0 then
-      dir="-X"
-    elseif x2-x1 == 0 and y2-y1 ==1 then
-      dir="Y"
-    elseif x2-x1 == 0 and y2-y1 ==-1 then
-      dir="-Y"
-    else
-      dir="something went terrible wrong"
-    end
-    return(dir)
-    end
+        end        
+    turtle.forward()
+  end
+end
 
-print(axis())
+turtle.refuel()
+
+x1 , y1, z1 = gps.locate( 2, false )
+
+-- desired pos
+x2=222
+y2=70
+z2=88
+
+-- find change in
+deltaX = x2-x1
+deltaY = y2-y1
+deltaZ = z1-z2
+print(deltaX,deltaY,deltaZ)
+
+-- go to X
+if deltaX > 0 then
+  turtle.turnRight()
+  fowardNum(deltaX)
+  turtle.turnLeft()
+elseif deltaX < 0 then
+  turtle.turnLeft()
+  fowardNum(math.abs(deltaX))
+  turtle.turnRight()
+end
+
+-- go to Z
+if deltaZ > 0 then
+  fowardNum(deltaZ)
+elseif deltaZ < 0 then
+  turtle.turnLeft()
+  turtle.turnLeft()
+  fowardNum(math.abs(deltaZ))
+  turtle.turnLeft()
+  turtle.turnLeft()
+end
+
+-- go to Y
+if deltaY > 0 then
+  for i=1, deltaY do
+    while turtle.detectUp() do
+      turtle.digUp()
+        end        
+    turtle.up()
+  end
+elseif deltaY < 0 then
+  for i=1, math.abs(deltaY) do
+    while turtle.detectDown() do
+      turtle.digDown()
+        end        
+    turtle.down()
+  end
+end
+
